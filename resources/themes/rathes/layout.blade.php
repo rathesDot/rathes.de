a103b45c28f7cda5cc0ff849b894014f7fe3fd7b
Rathes Sachchithananthan <rs@net-insiders.com>
2017-11-28 11:19:52 +0100

    Refactoring

--- resources/themes/rathes/layout.blade.php
+++ resources/themes/rathes/layout.blade.php
@@ -15,24 +15,7 @@
 </head>
 
 <body>
-	<div class="flex flex-wrap items-center p-6 justify-between bg-indigo md:px-16 md:bg-grey-lightest md:content-end
-        lg:px-32">
-        <div class="w-1/3 flex-no-shrink">
-            <img src="{{ asset('images/rathes.jpg') }}" alt="I am Rathes" title="I am Rathes"
-                class="rounded-full w-auto h-10 md:h-16">
-        </div>
-        <div class="block md:hidden">
-            <button class="js-toggle flex items-center px-2 py-2 border rounded text-white border-white">
-                <svg class="h-4 w-4" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-            </button>
-        </div>
-        <nav class="w-full mt-4 js-menu md:w-auto md:mt-0">
-            <a class="menu-link" href="/resume">Resume</a>
-            <a class="menu-link" href="/about">About</a>
-            <a class="menu-link" href="/work">Work</a>
-            <a class="menu-link" href="/blog">Blog</a>
-        </nav>
-	</div>
+    @include('theme::rathes.nav')
     @yield('content')
     <script src="{{ asset('/js/app.js') }}"></script>
 </body>
