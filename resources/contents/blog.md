5ffe0a29b5cc15dda85a6a13c06b08e17007530a
Rathes Sachchithananthan <rs@net-insiders.com>
2017-12-08 12:06:26 +0100

    Adds blog list

---
+++ resources/contents/blog.md
@@ -0,0 +1,5 @@
+---
+title: 'My writings'
+template: 'blog-list'
+---
+
---
+++ resources/themes/rathes/blog-list.blade.php
@@ -0,0 +1,18 @@
+@extends('theme::rathes.layout')
+
+@section('content')
+    <div class="container p-6 mx-auto md:px-16">
+        <h1>My Writings</h1>
+
+        <h2 class="mt-16 uppercase text-sm font-normal tracking-wide text-grey">2017</h2>
+
+        <ul class="list-reset mt-4">
+            <li class="my-4"><a href="#">Looking forward to 2018</a></li>
+            <li class="my-4"><a href="#">Wrting about some stuff</a></li>
+            <li class="my-4"><a href="#">JSON Webtoken - Wie man sie benutzt und was das ist</a></li>
+        </ul>
+
+        <h2 class="mt-16 uppercase text-sm font-normal tracking-wide text-grey">2016</h2>
+        <h2 class="mt-16 uppercase text-sm font-normal tracking-wide text-grey">Older</h2>
+    </div>
+@endsection
