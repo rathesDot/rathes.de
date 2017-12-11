e497b0d25bc52b6a4a67ff4f06957137ac4533d4
Rathes Sachchithananthan <rathes@aheenam.com>
2017-12-11 12:29:51 +0100

    Adds styles to homepage

--- resources/contents/index.md
+++ resources/contents/index.md
@@ -3,7 +3,13 @@ title: 'Das ist ein Test'
 template: index
 ---
 
-Hi there!
+Hello there!
 ===
 
-My name is **Rathes Sachchithananthan** and I am a former freelancing Web Designer and Web Developer. Currently you can find me working at [Aheenam](https://aheenam.com), the agency I started to provide digital solutions for anyone. Get in touch with me using social media!
+My name is **Rathes Sachchithananthan** and I'm a web developer currently living in Germany.
+
+My focus is developing apps using **Laravel**, **VueJS** & **React (Native)**, but I'm open for any topic in tech that sounds interesting.
+
+- Working at [netinsiders](https://netinsiders.de)
+- Living in [Schleswig](https://goo.gl/maps/sdJfBDU8Zs42)
+- Follow me [@rswebdesigner](https://twitter.com/rswebdesigner)
--- resources/themes/rathes/index.blade.php
+++ resources/themes/rathes/index.blade.php
@@ -1,11 +1,23 @@
 @extends('theme::rathes.layout')
 
 @section('content')
-    <main class="leading-normal overflow-auto">
-        <div class="container mx-auto px-6 leading-loose
-            md:px-16 md:text-xl md:h-3/4-screen md:flex items-center">
-            <div class="max-w-md">
+    <main class="overflow-auto">
+        <div class="container mx-auto px-6 leading-normal flex items-center min-h-screen
+            sm:px-16 sm:text-xl sm:h-3/4-screen sm:min-h-0">
+            <div class="max-w-sm py-8 home">
                 {!! $content !!}
+            </div>
+        </div>
+        <div class="flex flex-wrap">
+            <div class="bg-indigo-light w-1/2 h-64">
+            </div>
+            <div class="bg-indigo-lighter w-1/2">
+            </div>
+            <div class="bg-indigo-lightest h-64 w-1/2">
+                Test
+            </div>
+            <div class="bg-indigo w-1/2">
+                Test
             </div>
         </div>
     </main>
