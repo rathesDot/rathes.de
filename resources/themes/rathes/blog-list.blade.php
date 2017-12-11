e57e53c6177e4fe97aaa300adc1c5f1c4225f93c
Rathes Sachchithananthan <rathes@aheenam.com>
2017-12-11 13:46:10 +0100

    Add a max width for longer pages

--- resources/themes/rathes/blog-list.blade.php
+++ resources/themes/rathes/blog-list.blade.php
@@ -1,7 +1,9 @@
 @extends('theme::rathes.layout')
 
 @section('content')
-    <div class="container p-6 mx-auto md:px-16 blog-list">
-        {!! $content !!}
+    <div class="container mx-auto blog-list">
+        <div class="p-6 max-w-lg md:px-16">
+            {!! $content !!}
+        </div>
     </div>
 @endsection
--- resources/themes/rathes/page.blade.php
+++ resources/themes/rathes/page.blade.php
@@ -1,9 +1,8 @@
 @extends('theme::rathes.layout')
 
 @section('content')
-    <main class="leading-normal overflow-auto">
-        <div class="container mx-auto px-6 leading-normal
-            md:mt-16 md:px-16">
+    <main class="leading-normal overflow-auto container mx-auto">
+        <div class="px-6 leading-normal max-w-lg md:mt-16 md:px-16">
             <h1>{{ $meta['title'] }}</h1>
             {!! $content !!}
         </div>
