ea2401f403b4beb6091db95dc7fae909fa6aa374
Rathes Sachchithananthan <rathes@aheenam.com>
2017-12-11 13:37:13 +0100

    Adjusts page layout

--- resources/themes/rathes/page.blade.php
+++ resources/themes/rathes/page.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
     <main class="leading-normal overflow-auto">
-        <div class="container mx-auto px-6 leading-loose max-w-md md:mt-16">
+        <div class="container mx-auto px-6 leading-normal
+            md:mt-16 md:px-16">
+            <h1>{{ $meta['title'] }}</h1>
             {!! $content !!}
         </div>
     </main>
