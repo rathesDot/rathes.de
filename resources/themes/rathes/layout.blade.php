ffc5bdcf578001a1166d3a2526f8af622d7fd53f
Rathes Sachchithananthan <rathes@aheenam.com>
2017-12-17 20:34:24 +0100

    Adds Google Analytics

--- resources/themes/rathes/layout.blade.php
+++ resources/themes/rathes/layout.blade.php
@@ -18,6 +18,17 @@
 	/>
 
 	<link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-48222009-4"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-48222009-4');
+	</script>
+
 </head>
 
 <body>
