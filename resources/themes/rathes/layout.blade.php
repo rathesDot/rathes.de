4c94cf6020e43d255833bdfbd0d17905b6bbe390
Rathes Sachchithananthan <rathes@aheenam.com>
2017-12-17 20:37:38 +0100

    Adds Cookie Consent

--- resources/themes/rathes/layout.blade.php
+++ resources/themes/rathes/layout.blade.php
@@ -29,6 +29,14 @@
 		gtag('config', 'UA-48222009-4');
 	</script>
 
+    <!-- Begin Cookie Consent plugin by Silktide - http://silktide.com/cookieconsent -->
+    <script type="text/javascript">
+        window.cookieconsent_options = {"message":"Diese Webseite benutzt Cookies für eine optimierte Benutzererfahrung.","dismiss":"OK","learnMore":"Weitere Infos","link":"https://rathes.de/imprint/","theme":"dark-bottom"};
+    </script>
+
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.9/cookieconsent.min.js"></script>
+    <!-- End Cookie Consent plugin -->
+
 </head>
 
 <body>
