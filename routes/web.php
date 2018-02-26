06cf3a0577e63347f0fc25eb8937052806e123be
Rathes Sachchithananthan <rathes@aheenam.com>
2018-02-26 21:26:17 +0100

    Adds pdf version of resume

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,14 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/resume', function () {
+    return PDF::setOptions([
+            'dpi' => 300,
+            'defaultFont' => 'sans-serif',
+            'isHtml5ParserEnabled' => true,
+            'isFontSubsettingEnabled' => true
+        ])
+        ->loadView('resume')
+        ->stream('resume.pdf');
+});
