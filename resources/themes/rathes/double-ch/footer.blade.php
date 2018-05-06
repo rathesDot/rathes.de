4ca393feb8e489dc881bf45915e6a71a1f191cb7
Rathes Sachchithananthan <sachchi@rathes.de>
2018-05-06 16:30:46 +0200

    Fix layout has too much margin on mobile

--- resources/themes/rathes/double-ch/footer.blade.php
+++ resources/themes/rathes/double-ch/footer.blade.php
@@ -1,6 +1,6 @@
 
 <footer class="container mx-auto article-list text-xs text-grey">
-    <div class="p-6 max-w-md leading-normal ml-16 md:px-16 flex justify-between">
+    <div class="p-6 max-w-md leading-normal md:ml-16 md:px-16 flex justify-between">
         <div>
             &copy; 2017 - Rathes Sachchithananthan
         </div>
