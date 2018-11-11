12554c8083185b42fa66002a4e395e6550f33d3b
Rathes Sachchithananthan <sachchi@rathes.de>
2018-11-11 10:16:49 +0100

    💄 Adjust layout to look more consitent

--- resources/themes/rathes/blog-list.blade.php
+++ resources/themes/rathes/blog-list.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mx-auto blog-list text-grey-darker">
-        <div class="p-6 max-w-lg md:px-16">
+        <div class="px-6 leading-normal max-w-lg md:mt-16 md:px-16">
             {!! $content !!}
         </div>
     </div>
