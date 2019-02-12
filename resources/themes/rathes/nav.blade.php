416de7ec24b9be91c08c84bcf40c2565c0a44be3
Rathes Sachchithananthan <sachchi@rathes.de>
2019-02-12 19:38:37 +0100

    💄 Inline the navigation

--- resources/themes/rathes/nav.blade.php
+++ resources/themes/rathes/nav.blade.php
@@ -12,7 +12,7 @@
                 <svg class="h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
         </div>
-        <nav class="w-full mt-4 js-menu md:w-auto md:mt-0">
+        <nav class="w-full text-center bg-indigo-lightest rounded mt-4 js-menu md:bg-transparent md:w-auto md:mt-0">
             <a class="menu-link" href="/about">About</a>
             <a class="menu-link" href="/now">Now</a>
             <a class="menu-link" href="/writings">Writings</a>
