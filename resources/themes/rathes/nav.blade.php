ca5d43ce48c3886232e2cb4803b5ed9d414baa64
Rathes Sachchithananthan <sachchi@rathes.de>
2019-02-12 19:31:44 +0100

    💄 Invert the navigation colors

--- resources/themes/rathes/nav.blade.php
+++ resources/themes/rathes/nav.blade.php
@@ -1,14 +1,14 @@
-<div class="bg-indigo md:bg-transparent">
+<div>
 	<div class="container mx-auto flex flex-wrap items-center p-6 justify-between  md:px-16 md:content-end">
         <div class="w-1/3 flex-no-shrink">
-            <a href="{{ url('/') }}" class="font-logo text-white font-bold no-underline inline-block
-                md:text-indigo md:text-lg">
+            <a href="{{ url('/') }}" class="font-logo text-indigo font-bold no-underline inline-block
+                md:text-lg">
                 |) <br>
                 |\
             </a>
         </div>
         <div class="block md:hidden">
-            <button class="js-toggle flex items-center px-2 py-2 border rounded text-white border-white">
+            <button class="js-toggle flex items-center px-2 py-2 border rounded text-indigo border-indigo">
                 <svg class="h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
             </button>
         </div>
