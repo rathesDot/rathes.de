e7bf35c6276fb3a60022e42499b63c095c8a9457
Rathes Sachchithananthan <sachchi@rathes.de>
2019-02-19 23:40:36 +0100

    💄 Add maxWidth to heading

--- resources/themes/rathes/post.blade.php
+++ resources/themes/rathes/post.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container mx-auto leading-normal text-grey-darker px-6">
-        <header class="font-sans my-4 max-w-md">
+        <header class="font-sans my-4 max-w-580px">
             <h1 class="text-3xl font-medium leading-tight text-purple-darkest">
                 {{ $meta['title'] }}
             </h1>
