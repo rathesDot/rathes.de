1a203e73bd7656f8bf116bbd96e2fc42b28a6ffb
Rathes Sachchithananthan <sachchi@rathes.de>
2019-02-28 06:26:53 +0100

    🔥 Remove the response cache from providers --- It is auto discovered anyway

--- config/app.php
+++ config/app.php
@@ -159,7 +159,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Spatie\ResponseCache\ResponseCacheServiceProvider::class,
 
     ],
 
@@ -209,8 +208,6 @@ return [
         'URL'          => Illuminate\Support\Facades\URL::class,
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
-
-        'ResponseCache' => Spatie\ResponseCache\ResponseCacheFacade::class,
 
     ],
 
